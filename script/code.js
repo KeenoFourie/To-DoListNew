35fe4d675a44137ce8e71ac92fff1a76d4647fcb
KeenoFourie <keenofourie614@gmail.com>
2023-06-05 16:47:32 +0200

    javascript

--- script/code.js
+++ script/code.js
@@ -25,12 +25,36 @@ addItem.addEventListener("click", ()=> {
     output_list.innerHTML = ""
     to_DoList.forEach( (item) => {
         output_list.innerHTML += `
-            <div id="item_added"><input id="checkbox" type="checkbox"> ${item} <button id="clear_button">&times</button></div>
+            <div id="item_added"><input id="checkbox" type="checkbox"> ${item} 
+
+            <button type="button" class="btn bg-transparent" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Edit</button>
+            
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    ...
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Understood</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <button id="clear_button">&times</button></div>
         `
     })
 });
 
-let checkbox = document.querySelector("#checkbox")
-checkbox.addEventListener("click", ()=> {
+// Sort button
+
+sortItems.addEventListener("click", ()=> {
+    output_list
     
 })
